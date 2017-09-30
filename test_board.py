bd9506db6d75acf5f8fe89f0b4ebd9ce5f8ef4ef
Noufal Ibrahim <noufal@nibrahim.net.in>
2017-09-30 10:53:33 +0530

    RED : Adds a test to check a 4x4 board with all cells dead

--- test_board.py
+++ test_board.py
@@ -7,4 +7,12 @@ def test_display_all_dead_3x3():
     val = life.display(board)
     assert val == "...\n...\n..."
 
+def test_display_all_dead_4x4():
+    board = [[False, False, False, False],
+             [False, False, False, False],
+             [False, False, False, False],
+             [False, False, False, False]]
+    val = life.display(board)
+    assert val == "....\n....\n....\n...."
+
     
