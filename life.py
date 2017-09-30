723a0b0472cc8362f3d569542a438dbf174d150b
Noufal Ibrahim <noufal@nibrahim.net.in>
2017-09-30 11:27:32 +0530

    GREEN : Passes is_alive for dead cells

--- life.py
+++ life.py
@@ -10,4 +10,15 @@ def display(board):
         ret.append("".join(t_row))
     return "\n".join(ret)
         
-        
+
+def alive(board, row, col):
+    return False
+
+    
+
+# neighbours
+# kill
+# birth
+# keep_alive
+
+# if alive(cell)
