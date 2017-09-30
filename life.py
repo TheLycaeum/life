257b812fc96bf8786353cdd7ef74c383a84b26a7
Noufal Ibrahim <noufal@nibrahim.net.in>
2017-09-30 13:58:41 +0530

    Refactors num_neighbours

--- life.py
+++ life.py
@@ -23,36 +23,20 @@ def birth(board, row, col):
     board[row][col] = True
 
 def num_live_neighbours(board, row, col):
-    size = len(board)-1
-
-    if row == 0 and col == 0:
-        return int(board[0][1]) + int(board[1][1]) + int(board[1][0])
-
-    if row == 0 and col == size:
-        return int(board[0][size-1]) + int(board[1][size-1]) + int(board[1][size])
-    
-    if row == size and col == 0:
-        return int(board[size-1][0]) + int(board[size-1][1]) + int(board[size][1])
-        
-    if row == size and col == size:
-        return  int(board[size-1][size]) + int(board[size-1][size-1]) + int(board[size][size-1])
-
-    # Top edge
-    if row == 0:
-        return int(board[0][col-1]) + int(board[1][col-1]) + int(board[1][col]) + int(board[1][col+1]) + int(board[0][col+1])
-
-    if col == 0:
-        return int(board[row-1][0])+int(board[row-1][1])+int(board[row][1])+int(board[row+1][1])+int(board[row+1][0])
-
-    if row == size:
-        return int(board[size][col-1])+int(board[size-1][col-1])+int(board[size-1][col])+int(board[size-1][col+1])+int(board[size][col+1])
-
-    if col == size:
-        return int(board[row-1][col]) + int(board[row-1][col-1]) + int(board[row][col-1]) + int(board[row+1][col-1]) + int(board[row+1][col])
-
-    return  int(board[row-1][col-1])+ int(board[row-1][col])+ int(board[row-1][col+1])+ int(board[row][col+1])+ int(board[row+1][col+1])+ int(board[row+1][col])+ int(board[row+1][col-1])+ int(board[row][col-1])
-    
-
+    size = len(board) - 1
+    num = 0
+    for x in [-1, 0, 1]:
+        for y in [-1, 0, 1]:
+            nx, ny = row + x, col + y
+            if x == 0 and y == 0:
+                continue
+            if nx < 0 or nx > size:
+                continue
+            if ny < 0 or ny > size:
+                continue
+            num += int(board[nx][ny])
+    return num
+            
 
 def create_new_board(size):
     ret = []
